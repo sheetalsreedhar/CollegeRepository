029f6e180a72d922940827d995aa58d5e3aa6b99
Sheetal Sreedhar <sheetal.sreedhar@ibm.com>
2022-06-06 12:15:03 +0530

    The code for Entering StudentTeacher details

--- studentTeacher.java
+++ studentTeacher.java
@@ -1,11 +1,19 @@
+import java.util.*;
 public class studentTeacher
 {
 	public static void main(String args[])
 	{
+		String clgname;
+		String tname;
+		int id;
 		{
+			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter Your College name:");
+			clgname = sc.nextLine();
 			System.out.println("Enter Your TeachersName:");
+			tname = sc.nextLine();
 			System.out.println("Enter Your ID:");
+			id = sc.nextInt();
 		}
 	}
 }
