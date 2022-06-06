c77b1b594032ceba90e93bdd21fdebb81047a55e
Sheetal Sreedhar <sheetal.sreedhar@ibm.com>
2022-06-06 12:06:55 +0530

    Added datatypes

--- student.java
+++ student.java
@@ -1,11 +1,24 @@
+import java.util.*;
 public class student
 {
 	public static void main(String args[])
 	{
+		String clgname;
+		String name;
+		int id;
 		{
+			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter Your College name:");
+			clgname = sc.nextLine();
 			System.out.println("Enter Your ID:");
+			id = sc.nextInt();
 			System.out.println("Enter Your Name:");
+			name = sc.nextLine();
+			System.out.println("*******************");
+			System.out.println("Your College name is:"+clgname);
+			System.out.println("Your ID is":+id);
+			System.out.println("Your name is"+name);
+			
 		}
 	}
 }
