b5fe430378a6b0d9e19be574ade0ed0040c8be25
Sheetal Sreedhar <sheetal.sreedhar@ibm.com>
2022-06-06 11:50:40 +0530

    studentTeacher details

---
+++ studentTeacher.java
@@ -0,0 +1,11 @@
+public class studentTeacher
+{
+	public static void main(String args[])
+	{
+		{
+			System.out.println("Enter Your College name:");
+			System.out.println("Enter Your TeachersName:");
+			System.out.println("Enter Your ID:");
+		}
+	}
+}
